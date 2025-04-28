3e7552b028ca8830cfd437e6bf4ce3d6ad99a607
Abhijith Miriyala <abhijith.swamy.miriyala@celigo.com>
2025-04-28 12:50:49 +0530

    time added

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -15,5 +17,6 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
